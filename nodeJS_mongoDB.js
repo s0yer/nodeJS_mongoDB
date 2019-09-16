99c33886673e40dd5f98d492f62203520e074563
jadsonmarliere <jds.mdo@gmail.com>
2019-09-16 11:43:25 -0300

    Update nodeJS_mongoDB.js

--- nodeJS_mongoDB.js
+++ nodeJS_mongoDB.js
@@ -1,9 +1,9 @@
 
 
-var mongo = requrie('mongodb'); //module to manipulate mongoDB.
+var mongo = require('mongodb'); //module to manipulate mongoDB.
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/newDataBase"; // create a DB called 'newDataBase'
 
 //Mongo will create the DB if it does not exist, and make the connection.
 MongoClient.connect(url, function(err, db){
