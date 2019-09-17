6228bec7614cbd3df55e3a3a08357a4d3c33b198
jadsonmarliere <jds.mdo@gmail.com>
2019-09-17 10:25:38 -0300

    Update nodeJS_mongoDB.js
    Create a colection
    Insert a Object

--- nodeJS_mongoDB.js
+++ nodeJS_mongoDB.js
@@ -11,3 +11,25 @@ MongoClient.connect(url, function(err, db){
 	console.log("Database created!");
 	db.close();
 });
+
+MongoClient.connect(url, function(err,db){
+	if(err) throw err;
+	var dbo = db.db("newDataBase");
+	dbo.createCollection("clientes", function(err, res){ // create a collection called "clientes"
+		if(err) throw err;
+		console.log("Collection created!");
+		db.close();
+	});
+});
+
+MongoClient.connect(url, function(err ,db){
+	if(err) throw err;
+	var dbo = db.db("newDataBase");
+	var myObj = { name: " ELCA Instalações", address: "Campinas/São Paulo"}; // Object to be insertOne
+	dbo.collection("clientes").insertOne(myObj, function(err, res){ // inset the object in the 'clientes' collection
+		if(err) throw err;
+		console.log("Um contato adicionado!");
+		db.close;
+	});
+});
+	
